b9498a7539b43a070ae7398af6920361d4a23474
osamaghnam24 <osamaghnam24@gmail.com>
2024-02-28 00:48:47 +0200

    get Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,6 +3,26 @@ import styles from './Footer.module.css';
 
 export default function Footer() {
   return <>
-    <h1>Footer</h1>
-  </>
+<footer class="footer bg-dark text-light py-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h3>E-commerce</h3>
+                <p>This is a sample E-commerce. You can by any product from here.</p>
+            </div>
+            <div class="col-md-4">
+                
+            </div>
+            <div class="col-md-4">
+                <h3>Follow Us</h3>
+                <ul class="list-inline ">
+                    <li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                    <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href="#"><i class="fab fa-instagram"></i></a></li>
+                    <li class="list-inline-item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>  </>
 }
